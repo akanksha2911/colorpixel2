06f48d1da5d5dd75576b66a96d9eeb9c17e3f2b1
akanksha2911 <akanksha1913054@akgec.ac.in>
2020-12-25 11:21:11 +0530

    new_function

--- login.js
+++ login.js
@@ -1,6 +1,25 @@
-var http = require('http');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+var nodemailer = require('nodemailer');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'akankshaguptafzd2002@gmail.com',
+    pass: 'Akanksha@123'
+  }
+});
+
+var mailOptions = {
+  from: 'akankshaguptafzd2002@gmail.com',
+  to: '10as1913054@gmail.com',
+  subject: 'Sending Email using Node.js',
+  html: '<h1>Welcome</h1><p>That was easy!</p>'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
